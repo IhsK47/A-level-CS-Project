5bf63973bf5db7452c3616bc3320be959079a93f
IhsK47 <100438571+IhsK47@users.noreply.github.com>
2023-05-17 12:09:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import pygame
+import sys 
 
 pygame.init ()
 
 screen_width = 1280
 screen_height = 720
-#this is the screen size which is the HD size
-
-#font = pygame.font("arial") #defining fonts
-
+#this is the screen size which is set to be HD resolution
 
 screen = pygame.display.set_mode((screen_width, screen_height)) #initialise the display module
 
 running = True
-while running:
+while running: #this while loop allows a game loop to run
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #game quit
             pygame.quit()
             sys.exit()
             running = False
-#    pygame.display.update() #
+
+
+class Button():
+    ''
+
+
+
+pygame.font.init()
+
+font = pygame.font("arial") #defining fonts
