705402c2599982b2d688c1dbd687f0a57a96442a
IhsK47 <100438571+IhsK47@users.noreply.github.com>
2023-05-17 12:05:01 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ screen_width = 1280
 screen_height = 720
 #this is the screen size which is the HD size
 
-screen = 
+#font = pygame.font("arial") #defining fonts
 
+
+screen = pygame.display.set_mode((screen_width, screen_height)) #initialise the display module
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #game quit
+            pygame.quit()
+            sys.exit()
+            running = False
+#    pygame.display.update() #
