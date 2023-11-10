9d30bf549fe0873100c9aa9e267c83a56d685f51
IhsK47 <100438571+IhsK47@users.noreply.github.com>
2023-11-10 16:23:05 +0000

    Add files via upload

--- gamePlay.py
+++ gamePlay.py
@@ -15,7 +15,7 @@ def background():
     #loading background
     sky_surface = pygame.image.load('graphics\sunsetPixel.png').convert_alpha() #importing the image
     sky_surface = pygame.transform.scale(sky_surface, (screen_size)) #resizing to screen size
-    screen.blit(sky_surface, (0,0) )
+    screen.blit(sky_surface, (0,0) ) # 0,0 starts at bottom right 
 
 def overlay ():
     overlay = pygame.Surface(screen_size) #instantiating an overlay to soften image for main menu
@@ -36,10 +36,11 @@ class Castle ():
         self.health = 1000
         self.max_health = self.health
     
-        width = castleSprite.get_width () 
-        height = castleSprite.get_height ()
+        width = int(castleSprite.get_width () * scale)
+        height = int(castleSprite.get_height ()  * scale)
+        print ('castle w.h:', width, height)
 
-        self.image = pygame.transform.scale(image, (int(width*scale)) , (height*scale) ) #to ensure image is a correct size
+        self.castleSprite = pygame.transform.scale(castleSprite, (height, width) ) #to ensure image is a correct size
         self.rect = self.castleSprite.get_rect()
         self.rect.x, self.rect.y = x, y #x&y co-odinates for the rect
 
@@ -47,13 +48,19 @@ class Castle ():
     def setHealth (self, health): #set health via castle upgrades
         self.health = health
 
+    def draw (self):
+        self.image = self.castleSprite
+        screen.blit (self.image, self.rect) #co ordinated defined already for x & y hence self.rect can be passed in
 
+# create castle (castleSprite,x,y, scale)
+castle = Castle (castleSprite,0, 380 ,0.7 ) 
 
 
 running = True
 while running: #this while loop allows a game loop to run
 
     background()
+    castle.draw ()
 
     pos = pygame.mouse.get_pos()
     pygame.display.update()
