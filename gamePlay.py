ff29203d64c0cd31f5b91538d14435434f3f117b
IhsK47 <100438571+IhsK47@users.noreply.github.com>
2023-11-14 17:55:12 +0000

    Add files via upload

--- gamePlay.py
+++ gamePlay.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_caption('My very cool game') #giving the window a na
 screen = pygame.display.set_mode(screen_size) #initialise the display module/object
 clock = pygame.time.Clock() #intantiating the clock object
 
+player_team = 'teamBarie'
+enemies_team = 'Opps'
+
 
 def background(): 
     #loading background
@@ -24,9 +27,10 @@ def overlay ():
     screen.blit(overlay, (0,0) )
 
 
-
 #load images
 castleSprite = pygame.image.load('graphics\castle 3d.png').convert_alpha() #castle
+barrieSprite = pygame.image.load('grahics\barrieStickman.png').convert_alpha() #main character
+
 
 
 #castle class
@@ -51,6 +55,44 @@ class Castle ():
     def draw (self):
         self.image = self.castleSprite
         screen.blit (self.image, self.rect) #co ordinated defined already for x & y hence self.rect can be passed in
+
+
+class Barrie (): #the main character of my game is called barrie
+    
+    def __init__ (self,barrieSprite,x, y, scale):
+        self.health = 200
+        self.max_health = self.health
+        self.move_speed = 5
+
+        width = int(barrieSprite.get_width () * scale)
+        height = int(barrieSprite.get_height ()  * scale)
+        print ('barrie w.h:', width, height)
+
+        self.barrieSprite = pygame.transform.scale(barrieSprite, (height, width) ) #to ensure image is a correct size
+        self.rect = self.barrieSprite.get_rect()
+        self.rect.x, self.rect.y = x, y #x&y co-odinates for the rect
+
+    def movement (self):
+        pass #to be completed later
+
+    def setHealth (self, health): #set health via castle upgrades
+        self.health = health
+
+    def shoot(self):
+        pass # to be completed later
+
+
+    def draw (self):
+        self.image = self.barrieSprite
+        screen.blit (self.image, self.rect) #co ordinated defined already for x & y hence self.rect can be passed in
+
+
+class Soilders (pygame.sprite.Sprite):
+
+    def __init__ (self, x, y, unit_type, soilder_team, side, vetical, walk_frames, attack_frames, health, attack, defence, speed): 
+        pass
+
+
 
 # create castle (castleSprite,x,y, scale)
 castle = Castle (castleSprite,0, 380 ,0.7 ) 
