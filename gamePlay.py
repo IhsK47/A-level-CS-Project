76b8301fd297f049af4a2a1fe8f0a26556730da0
IhsK47 <100438571+IhsK47@users.noreply.github.com>
2023-12-11 15:22:30 +0000

    Add files via upload
    plus allurium etc

--- gamePlay.py
+++ gamePlay.py
@@ -35,7 +35,7 @@ castleSprite = pygame.image.load('graphics\castle 3d.png').convert_alpha() #cast
 barrieSprite = pygame.image.load('graphics\BarrieStickman.png').convert_alpha() #main character
 
 #creating a rect for enemy testing 
-enemySprite = pygame.Rect(  screen_width-10 , 460, 100,100   )
+enemySprite = pygame.Rect( screen_width-10 , 460, 100,100   )
 
 #castle class
 
@@ -61,13 +61,19 @@ class Castle ():
         screen.blit (self.image, self.rect) #co ordinated defined already for x & y hence self.rect can be passed in
 
 
-class Barrie (): #the main character of my game is called barrie
+
+class Barrie (pygame.sprite.Sprite): #the main character of my game is called barrie
     
     def __init__ (self,barrieSprite,x, y, scale):
+        
+        pygame.sprite.Sprite.__init__ (self)
+        self.barrieSprite = barrieSprite
         self.health = 200
         self.max_health = self.health
         self.speed = 15
         self.fired = False
+
+        self.allurium = 0
 
         width = int(barrieSprite.get_width () * scale)
         height = int(barrieSprite.get_height ()  * scale)
@@ -77,23 +83,21 @@ class Barrie (): #the main character of my game is called barrie
         self.rect.x, self.rect.y = x, y #x&y co-odinates for the rect
 
     def move (self):
-        # if pos < screen width: #barriers
-#        self.rect.move_ip(0,speed)
 
         for event in pygame.event.get():
             key = pygame.key.get_pressed()
-            if (key[pygame.K_a] == True) or (key[pygame.K_DOWN] == True):
-                self.rect.move_ip(-self.speed, 0)
-            if (key[pygame.K_d] == True) or (key[pygame.K_UP] == True):
-                self.rect.move_ip(self.speed, 0)
-        pygame.display.flip()
-
+            if (self.rect.left > 0)  and ( (key[pygame.K_a] == True) or (key[pygame.K_DOWN] == True) ) :
+                self.rect.move_ip(-self.speed, 0 )
+            if (self.rect.right < screen_width)  and  (key[pygame.K_d] == True) or (key[pygame.K_UP] == True):
+                self.rect.move_ip(self.speed, 0 )
+        #pygame.display.flip()
 
     def setHealth (self, health): #set health via castle upgrades
         self.health = health
 
     def draw (self):
         self.image = self.barrieSprite
+        pygame.draw.rect (screen, white, self.rect, 1) #########################################
         screen.blit (self.image, self.rect) #co ordinated defined already for x & y hence self.rect can be passed in
 
     def shoot(self): #created bullets using bullets class
@@ -101,7 +105,7 @@ class Barrie (): #the main character of my game is called barrie
         pos = pygame.mouse.get_pos()
 
         bulletStart_x = self.rect.right - 5
-        bulletStart_y = self.rect.top + 20
+        bulletStart_y = self.rect.top + 25
         x_dist = pos[0] - bulletStart_x
         y_dist = bulletStart_y - pos[1]
         self.angle = math.atan2 (y_dist, x_dist)
@@ -114,9 +118,39 @@ class Barrie (): #the main character of my game is called barrie
             self.fired = False
 
         
-        pygame.draw.line (screen, white, (bulletStart_x, bulletStart_y)  ,  (pos) )
+        pygame.draw.line (screen, white, (bulletStart_x, bulletStart_y)  ,  (pos) ) #############################
 
-        pygame.display.flip ()
+    def allurium_pickUp(self):
+        print ('alluriumpu running',self.allurium)
+        for allurium_sprite in pygame.sprite.spritecollide(self,allurium_group, True): #setting to true will automatically delete the allurium that has collided
+            print ('allurium hit')
+            self.allurium += allurium_sprite.quantity
+
+    def update (self):
+        self.draw ()
+        self.move()
+        self.allurium_pickUp ()
+        self.shoot()
+
+
+class Allurium(pygame.sprite.Sprite):
+    def __init__ (self, x ):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('graphics\Allurium_bars.webp').convert_alpha() #load bullet
+        self.image = scale (self.image,1)
+
+        self.quantity = 3
+
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, 700 #x&y co-odinates for the rect
+
+    def draw (self): #spawn allurium
+
+        screen.blit (self.image, self.rect) #co ordinated defined already for x & y hence self.rect can be passed in
+
+    def update (self):
+        self.draw()
+
         
 class Bullet (pygame.sprite.Sprite):
 
@@ -129,7 +163,7 @@ class Bullet (pygame.sprite.Sprite):
         height = int(self.image.get_height ()  * self.scale)
         self.image = pygame.transform.scale(self.image, (width, height) ) #to ensure image is a correct size
 
-        self.speed = 9
+        self.speed = 8
         self.angle = angle
 
         #horizontal and vertical speeds using trig 
@@ -137,7 +171,7 @@ class Bullet (pygame.sprite.Sprite):
         self.dy = - (math.sin(self.angle) * self.speed)
 
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = x, y #x&y co-odinates for the rect
+        self.rect.x, self.rect.y = (x - width), y #x&y co-odinates for the rect
     
     def update (self):
 
@@ -145,47 +179,85 @@ class Bullet (pygame.sprite.Sprite):
             self.kill ()
         self.rect.move_ip(self.dx, 0)
         self.rect.move_ip(0, self.dy)
+
+
+class Enemy (pygame.sprite.Sprite): 
+
+    def __init__ (self, x, enemy_type, walk_frames, attack_frames, attack, defence, speed): 
         
-
-
-class Soldiers (pygame.sprite.Sprite): 
-
-    def __init__ (self, x, y, soldier_type, soldier_team, side, walk_frames, attack_frames, health, attack, defence, speed): 
         pygame.sprite.Sprite.__init__ (self)
 
-        self.soldier_type = soldier_type
-        self.soldier_team = soldier_team
+        if enemy_type == 'swordman':
+            self.enemySprite = pygame.image.load('graphics\enemySprite.png').convert_alpha()            
+            self.scale = 0.21
+            self.health = 80
 
-        self.enemySprite = pygame.image.load('graphics\enemySprite.png').convert_alpha()
+        self.image = self.enemySprite
+        width = int(self.image.get_width () * self.scale)
+        height = int(self.image.get_height ()  * self.scale)
+        self.image = pygame.transform.scale(self.image, (width, height) ) #to ensure image is a correct size
         
-        if self.soldier_team == enemies_team:
-            self.image = self.enemySprite
-            self.scale = 0.3
-            width = int(self.image.get_width () * self.scale)
-            height = int(self.image.get_height ()  * self.scale)
-            self.image = pygame.transform.scale(self.image, (width, height) ) #to ensure image is a correct size
+        self.rect = self.image.get_rect()
 
+        self.rect.left, self.rect.y = x, 525 # y pos
 
-
-        self.side = side #left/right
-        self.vertical = 480 # y pos
-        
-        self.walkingAnimationLength = walk_frames
-        self.attackingAnimationLength = attack_frames
-
-        self.health = health
         self.attack = attack
-        self.defence = defence
+        self.attack_cooldown = 900
+        self.attackingAnimationLength = attack_frames
+        self.last_attack = pygame.time.get_ticks()
+
         self.speed = speed
+        self.walkingAnimationLength = walk_frames
+
+        self.defence = defence
         self.alive = True
+
+        self.action_list = ['walk','attackP','attackB','death'] #attackP = attacking player, attackB = attacking base
+        self.current_action = 0 # 0: walk, 1: attackB, 2: attackP 3: death
 
         self.last_update = 0 #when amimation was last updated
         self.current_frame = 0 #current frame for animation, starts at zero to be on the first frame
+        self.update_time = pygame.time.get_ticks()
+        
         self.load_animations()
 
+    def check_collisions():
+        pass
 
+    def move (self):
+        self.rect.x -= self.speed
+        screen.blit(self.image,self.rect)
 
-    def load_animations ():
+    def update (self):
+
+        if self.health <= 0:
+            self.alive = False
+            self.update_action (0) #die
+            print ('enemy killed')
+            self.kill ()
+        else:
+            if pygame.sprite.spritecollide(self,bullet_group, True): #setting to true will automatically delete the bullet that has collided
+                print ('enemy hit')
+                self.health -= 20
+
+            if self.rect.left < barrie.rect.right and not(self.rect.right < barrie.rect.left): #check if enemy has reached barrie 
+                self.update_action (1) #attack barrie
+                print ('enemy is attacking barrie')
+
+            elif self.rect.left < castle.rect.right: #check if enemy has reached castle 
+                self.update_action (2) #attack base
+                print ('enemy is attacking base')
+
+            elif self.alive: #only moves if alive
+                self.move()
+                
+    def update_action (self, new_action): #check if the new action is different to the previous one
+        if new_action != self.current_action:
+            self.current_action = new_action
+            self.current_frame = 0 #this ensures frame index is returned to 0 to begin on the animations for the next action rather than starting halfway through the other actions
+            self.update_date = pygame.time.get_ticks () 
+
+    def load_animations (self):
         pass #to be completed later
     ''' #this code below ensures that the first frame is the first frame of walking with is in animation list as zero,zero 
     self.animation_list = animation_list 
@@ -194,10 +266,13 @@ class Soldiers (pygame.sprite.Sprite):
     self.image = self.animation_list [self.action][self.frame_index]
     '''
 
+def scale (image, scale):
+    width = int(image.get_width () * scale)
+    height = int(image.get_height ()  * scale)
+    print (width, height)
+    image = pygame.transform.scale(image, (width, height)) #to ensure image is a correct size
+    return image
 
-class Swordman (pyame.sprite.Sprite):
-    def __init__ ():
-        pass 
 
 
 # create castle (castleSprite,x,y, scale)
@@ -205,8 +280,21 @@ castle = Castle (castleSprite,0, 380 ,0.7 )
 #create main character 
 barrie = Barrie (barrieSprite, 250, 480, 0.25)
 
+#barrie group
+barrie_group = pygame.sprite.GroupSingle()
+barrie_group.add(barrie)
+
 #create groups 
 bullet_group = pygame.sprite.Group ()
+enemy_group = pygame.sprite.Group ()
+allurium_group = pygame.sprite.Group ()
+#allurium test
+allurium1 = Allurium (500)
+allurium_group.add(allurium1)
+
+#create enemy
+enemy1 = Enemy (screen_width - 100, 'swordman',10, 10,10, 10, 4)
+enemy_group.add(enemy1)
 
 
 
@@ -217,22 +305,25 @@ while running: #this while loop allows a game loop to run
     background.draw()
     castle.draw ()
 
-    barrie.draw ()
-    barrie.move()
-    barrie.shoot()
+    barrie_group.update()
 
-    bullet_group.draw (screen)
+    allurium_group.update()
+
     bullet_group.update ()
+    bullet_group.draw (screen) 
+
+    enemy_group.update ()
+    enemy_group.draw (screen)
 
     pos = pygame.mouse.get_pos()
+
     pygame.display.update()
     for event in pygame.event.get(): 
-        print (event)
+        print (event) #to allow me see when and which events are being considered
+        print (pygame.time.get_ticks ())
         if event.type == pygame.QUIT:
             running = False
 
-
-
-
+    clock.tick(60)
 
 pygame.quit()
